ed9fe306ed5dcb6c73986c37a5f26d79eb2dd4bd
Alexey Ivanov <aivan1970@yandex.ru>
2022-08-22 13:54:18 +0500

    signup/5

--- backend/app.js
+++ backend/app.js
@@ -44,13 +44,13 @@ app.use((req, res, next) => {
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['Access-Control-Request-Headers'];
+  // const requestHeaders = req.headers['Access-Control-Request-Headers'];
   // Если это предварительный запрос, добавляем нужные заголовки
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,content-type');
     // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
