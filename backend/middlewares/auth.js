f45319bc8a09373e6e1029656a87d896a0663276
Alexey Ivanov <aivan1970@yandex.ru>
2022-09-03 13:30:51 +0500

    cors2

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../utils/errors/unauthorized');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
@@ -12,7 +16,7 @@ module.exports.auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (e) {
     next(new UnauthorizedError('Необходима авторизация'));
 
